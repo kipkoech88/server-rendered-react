cbbce8663b5319b88745e7adb42736e848c4403e
kipkoech88 <hoseakipkoech88@gmail.com>
2023-08-31 20:43:20 +0300

    added functionality to servered rendered app

--- client/App.jsx
+++ client/App.jsx
@@ -1,10 +1,25 @@
 import React from "react";
 
 
-export const App = ()=>{
+export const App = ({questions, answers})=>{
     return (
         <div>
-            react component
+            <h1>Q&A Tool</h1>
+
+            {questions.map(({questionId, content})=>(
+                <div key={questionId}>
+                    <h1>{content}</h1>
+                    <div>
+                    {answers.filter(answer => answer.questionId === questionId).map(({answerId, content, upvotes})=>(
+                        <div key={answerId}>
+                                <span>
+                                    {content} - {upvotes}
+                                </span>
+                        </div>
+                    ))}
+                    </div>
+                </div>
+            ))}
         </div>
     )
     
--- server/index.js
+++ server/index.js
@@ -7,6 +7,32 @@ import {renderToString} from 'react-dom/server';
 
 import { App } from '../client/App'; 
 
+const data = {
+    questions:[
+        {
+            questionId:"q1",
+            content:"should we use JQuery, fetch or Ajax"
+        },
+        {
+            questionId: "Q2",
+            content:"What is the best thing about React"
+        }
+    ],
+    answers:[{
+        answerId: "A1",
+        questionId: "Q1",
+        upvotes: 2,
+        content: "JQuery"
+    },
+    {
+        answerId: "A2",
+        questionId: "Q2",
+        upvotes: 1,
+        content: "Fetch"
+    } 
+]
+}
+
 const app = express();
 
 app.use(express.static("dist"));
@@ -14,7 +40,7 @@ app.use(express.static("dist"));
 app.get("/", (req, res)=>{
     const index = readFileSync("./public/index.html", "utf-8");
 
-    const rendered = renderToString(<App/>);
+    const rendered = renderToString(<App {... data} />);
 
     res.send(index.replace("{{rendered}}", rendered));
 });

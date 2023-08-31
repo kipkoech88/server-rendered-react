03fe7c8e37b8ef498c01ca40c8b8c9d391f143b8
kipkoech88 <hoseakipkoech88@gmail.com>
2023-08-31 19:55:31 +0300

    fixed webpack dependency issues, added webpack config and updated server

--- client/client.jsx
+++ client/client.jsx
@@ -2,9 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = ()=>{
-    <div>
-        react component
-    </div>
+    return (
+        <div>
+            react component
+        </div>
+    )
+    
 }
 
 
--- server/index.js
+++ server/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
+import {readFileSync} from 'fs'
 
 const app = express();
 
+app.use(express.static("dist"));
+
 app.get("/", (req, res)=>{
-    res.send("<h1>React is awesome!</h1>");
+    const index = readFileSync("./public/index.html", "utf-8");
+
+    res.send(index);
 });
 
 app.listen(777, ()=>{

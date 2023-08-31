b8a8d70689f97c333d1d69edd2f4063f2b0d2156
kipkoech88 <hoseakipkoech88@gmail.com>
2023-08-31 18:41:11 +0300

    updated script

---
+++ client/client.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = ()=>{
+    <div>
+        react component
+    </div>
+}
+
+
+ReactDOM.render(<App/>, 
+document.querySelector("#Container"))
